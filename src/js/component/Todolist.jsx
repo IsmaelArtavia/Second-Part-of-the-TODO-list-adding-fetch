0dd5d2fc295f837474728917183dfa8f2fc1b73a
IsmaelArtavia <ismaelartavia@yahoo.es>
2021-02-11 21:51:18 +0000

    todos con fetch

--- src/js/component/Todolist.jsx
+++ src/js/component/Todolist.jsx
@@ -28,6 +28,29 @@ const Todolist = () => {
 
 		setListaTareas(newList);
 	}
+
+	fetch("https://assets.breatheco.de/apis/fake/todos/user/ismaelartavia", {
+		method: "PUT",
+		body: JSON.stringify(listaTareas),
+		headers: {
+			"Content-Type": "application/json"
+		}
+	})
+		.then(resp => {
+			//console.log(resp.ok); // will be true if the response is successfull
+			//console.log(resp.status); // the status code = 200 or code = 400 etc.
+			//console.log(resp.text()); // will try return the exact result as string
+			return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+		})
+		.then(data => {
+			//here is were your code should start after the fetch finishes
+			//console.log(data); //this will print on the console the exact object received from the server
+		})
+		.catch(error => {
+			//error handling
+			//console.log(error);
+		});
+
 	return (
 		<div className="container">
 			<h1>Todos</h1>
